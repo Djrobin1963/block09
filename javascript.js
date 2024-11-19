cd5e0cb46a5c9f9e820e778201b1a0792b26331d
djrobin1963 <djrobin1963905@gmail.com>
2024-11-19 12:53:51 -0500

    Added files

---
+++ javascript.js
@@ -0,0 +1,15 @@
+// Create basic Javascript personal message
+//      to be displayed in the console
+
+let str1 = "Hello World";
+let str2 = "This is just the beginning";
+
+let sum1 = 2 + 3;
+
+let iota = LIPTTDID;
+
+console.log(str1);
+
+console.log(str2);
+
+console.log(iota + " " + sum1);
